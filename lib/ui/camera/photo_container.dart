7cb4814611acf8de7da445f762c5dedc2677809e
LennardDeurman <lennarddeurman@live.nl>
2021-04-17 14:33:18 +0200

    Refactored PhotoContainer class, _CloseButton apart from other button

--- lib/ui/camera/photo_container.dart
+++ lib/ui/camera/photo_container.dart
@@ -2,24 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:foodage/ui/extensions.dart';
 import 'package:foodage/ui/widgets/fdg_ratio.dart';
 
+class _CloseButton extends StatelessWidget {
+
+  final double size;
+  final double buttonInnerMargin;
+  final WidgetTapCallback onRemovePressed;
+
+  _CloseButton ({ @required this.size, @required this.onRemovePressed, this.buttonInnerMargin = 5 });
+
+  double get _borderRadius => size / 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(_borderRadius),
+        child: Material(
+          color: Theme.of(context).primaryColor,
+          child: InkWell(
+            child: Container(
+              child: Center(
+                child: Icon(Icons.close, color: Colors.white, size: size - buttonInnerMargin),
+              ),
+            ),
+            onTap: () => onRemovePressed(context),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
 class PhotoContainer extends StatelessWidget {
 
   final WidgetTapCallback onRemovePressed;
+  final double borderRadius;
 
-  PhotoContainer ({ this.onRemovePressed });
+  static const _backgroundColor = Color.fromRGBO(100, 100, 100, 1);
+  static const _closeButtonSize = 20.0;
+
+
+  PhotoContainer ({ this.borderRadius = 10, this.onRemovePressed });
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5,
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10)
+          borderRadius: BorderRadius.circular(borderRadius)
       ),
       child: FDGRatio(
         child: Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Color.fromRGBO(100, 100, 100, 1),
+            borderRadius: BorderRadius.circular(borderRadius),
+            color: _backgroundColor,
           ),
           child: Stack(
             clipBehavior: Clip.none,
@@ -27,23 +66,9 @@ class PhotoContainer extends StatelessWidget {
               Positioned(
                 right: 10,
                 top: -8,
-                child: Container(
-                  width: 20,
-                  height: 20,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Material(
-                      color: Theme.of(context).primaryColor,
-                      child: InkWell(
-                        child: Container(
-                          child: Center(
-                            child: Icon(Icons.close, color: Colors.white, size: 14),
-                          ),
-                        ),
-                        onTap: () => onRemovePressed(context),
-                      ),
-                    ),
-                  ),
+                child: _CloseButton(
+                  size: _closeButtonSize,
+                  onRemovePressed: onRemovePressed,
                 ),
               )
             ],
