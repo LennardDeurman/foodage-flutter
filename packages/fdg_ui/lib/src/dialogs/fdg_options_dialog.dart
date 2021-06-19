efbea5e9e9487cff82b47e6401ce085e184db6f0
Lennard Deurman <lennarddeurman@live.nl>
2021-06-19 02:00:52 +0200

    Store picker:
    - Added footer and header options
    - Added localizations

--- packages/fdg_ui/lib/src/dialogs/fdg_options_dialog.dart
+++ packages/fdg_ui/lib/src/dialogs/fdg_options_dialog.dart
@@ -8,9 +8,13 @@ class FDGOptionsDialog<T> extends StatelessWidget {
   final List<T> options;
   final T? value;
   final String Function(T)? label;
+  final Widget? footer;
+  final Widget? header;
 
   FDGOptionsDialog({
     required this.options,
+    this.footer,
+    this.header,
     this.value,
     this.label,
   });
@@ -18,6 +22,15 @@ class FDGOptionsDialog<T> extends StatelessWidget {
   String _label(BuildContext context, T option) {
     if (label != null) return label!(option);
     return option.toString();
+  }
+
+  Widget _rowContent(BuildContext context, int index) {
+    final option = options[index];
+    return FDGOptionsItemTile<T>(
+      option: option,
+      label: (option) => _label(context, option),
+      isSelected: option == value,
+    );
   }
 
   @override
@@ -34,58 +47,156 @@ class FDGOptionsDialog<T> extends StatelessWidget {
               itemCount: options.length,
               shrinkWrap: true,
               itemBuilder: (context, index) {
-                final option = options[index];
-                return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border(
-                      bottom: BorderSide(
-                        width: 1,
-                        color: FDGTheme().colors.lightGrey2,
-                      ),
-                    ),
-                  ),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      child: Container(
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Container(
-                                child: Text(
-                                  _label(context, option),
-                                  style: Theme.of(context).textTheme.headline3,
-                                ),
-                                padding: EdgeInsets.all(20),
-                              ),
-                            ),
-                            Visibility(
-                              child: Container(
-                                margin: EdgeInsets.only(right: 20),
-                                child: Icon(
-                                  Icons.done,
-                                  size: 24,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
-                              visible: option == value,
-                            )
-                          ],
-                        ),
-                      ),
-                      onTap: () => Navigator.pop(
-                        context,
-                        option,
-                      ),
-                    ),
-                  ),
-                );
+                final isFirstPosition = index == 0;
+                final isLastPosition = index == options.length - 1;
+                if (isFirstPosition) {
+                  return Column(
+                    children: [
+                      if (header != null) header!,
+                      _rowContent(context, index),
+                    ],
+                  );
+                } else if (isLastPosition) {
+                  return Column(
+                    children: [
+                      _rowContent(context, index),
+                      if (footer != null) footer!,
+                    ],
+                  );
+                }
+
+                return _rowContent(context, index);
               },
             ),
           ),
           borderRadius: BorderRadius.circular(20),
         ),
+      ),
+    );
+  }
+}
+
+class FDGOptionsItemTile<T> extends StatelessWidget {
+  static const _spacing = 20.0;
+  static const _iconSize = 24.0;
+
+  final String Function(T)? label;
+  final T option;
+  final bool isSelected;
+
+  const FDGOptionsItemTile({
+    this.label,
+    required this.option,
+    this.isSelected = false,
+    Key? key,
+  }) : super(key: key);
+
+  String _label(BuildContext context, T option) {
+    if (label != null) return label!(option);
+    return option.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            width: 1,
+            color: FDGTheme().colors.lightGrey2,
+          ),
+        ),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          child: Container(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    child: Text(
+                      _label(context, option),
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                    padding: EdgeInsets.all(_spacing),
+                  ),
+                ),
+                Visibility(
+                  child: Container(
+                    margin: EdgeInsets.only(right: _spacing),
+                    child: Icon(
+                      Icons.done,
+                      size: _iconSize,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  visible: isSelected,
+                )
+              ],
+            ),
+          ),
+          onTap: () => Navigator.pop(
+            context,
+            option,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FDGOptionsAddTile extends StatelessWidget {
+  final Widget label;
+  final Widget? icon;
+
+  static const _spacing = 20.0;
+  static const _iconSize = 24.0;
+
+  FDGOptionsAddTile({
+    required this.label,
+    this.icon,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        child: Container(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  child: DefaultTextStyle(
+                    style: theme.textTheme.headline3!.copyWith(
+                      fontStyle: FontStyle.italic,
+                      color: FDGTheme().colors.lightGrey1,
+                    ),
+                    child: label,
+                  ),
+                  padding: EdgeInsets.all(_spacing),
+                ),
+              ),
+              if (icon != null)
+                Container(
+                  margin: EdgeInsets.only(right: _spacing),
+                  child: IconTheme(
+                    data: IconThemeData(
+                      size: _iconSize,
+                      color: theme.primaryColor,
+                    ),
+                    child: icon!,
+                  ),
+                ),
+            ],
+          ),
+        ),
+        onTap: () {},
       ),
     );
   }
--- packages/fdg_web_admin/lib/src/ui/products/store_picker/store_picker.dart
+++ packages/fdg_web_admin/lib/src/ui/products/store_picker/store_picker.dart
@@ -1,5 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:fdg_common/fdg_common.dart';
 import 'package:fdg_ui/fdg_ui.dart';
+import 'package:fdg_web_admin/src/fdg_products_locale_keys.dart';
 import 'package:fdg_web_admin/src/models/store.dart';
 import 'package:fdg_web_admin/src/ui/products/store_picker/cubit/store_picker_cubit.dart';
 import 'package:flutter/material.dart';
@@ -7,9 +9,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class StorePicker extends StatelessWidget {
   final Store? initialValue;
+  final bool shouldShowAlert;
 
   const StorePicker({
     Key? key,
+    this.shouldShowAlert = false,
     this.initialValue,
   }) : super(key: key);
 
@@ -18,10 +22,35 @@ class StorePicker extends StatelessWidget {
     return BlocConsumer<StorePickerCubit, ResourceState<List<Store>>>(
       builder: (context, state) {
         if (state.data != null) {
+          final theme = Theme.of(context);
           return FDGOptionsDialog<Store>(
             options: state.data!,
             value: initialValue,
             label: (product) => product.name,
+            header: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  if (shouldShowAlert) _StorePickerInlineAlert(),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: FDGProductsLocaleKeys.storePickerSearchHint.tr(),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            footer: FDGOptionsAddTile(
+              label: Text(
+                FDGProductsLocaleKeys.storePickerAddNew.tr(),
+              ),
+              icon: Icon(Icons.add),
+            ),
           );
         }
         return Container(
@@ -39,6 +68,7 @@ class StorePicker extends StatelessWidget {
   static void show(
     BuildContext context, {
     Store? initialValue,
+    bool shouldShowAlert = false,
   }) {
     final storePickerCubit = context.read<StorePickerCubit>();
     showDialog<Store>(
@@ -47,9 +77,54 @@ class StorePicker extends StatelessWidget {
       builder: (context) => BlocProvider.value(
         value: storePickerCubit,
         child: StorePicker(
+          shouldShowAlert: shouldShowAlert,
           initialValue: initialValue,
         ),
       ),
     );
   }
+}
+
+class _StorePickerInlineAlert extends StatelessWidget {
+
+  static const _padding = EdgeInsets.all(15);
+  static const _innerSpacing = 5.0;
+  static const _borderRadius = 10.0;
+  static const _spacing = 10.0;
+
+  const _StorePickerInlineAlert ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      margin: EdgeInsets.symmetric(
+        vertical: _spacing,
+      ),
+      padding: _padding,
+      decoration: BoxDecoration(
+        color: FDGTheme().colors.lightGrey3,
+        borderRadius: BorderRadius.circular(_borderRadius),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            FDGProductsLocaleKeys.storePickerInfoTitle.tr(),
+            style: theme.textTheme.headline3!,
+          ),
+          SizedBox(
+            height: _innerSpacing,
+          ),
+          Text(
+            FDGProductsLocaleKeys.storePickerInfoDescription.tr(),
+            style: theme.textTheme.subtitle1!.copyWith(
+              color: FDGTheme().colors.grey.withOpacity(0.7),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
